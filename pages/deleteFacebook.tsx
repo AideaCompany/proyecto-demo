8d5a57cd055239c79f1a84893d27205278d62f15
Johan <johanavilax@gmail.com>
2021-11-20 16:43:49 -0500

    first commit

---
+++ pages/deleteFacebook.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const deleteFacebook = () => {
+    return (
+        <div style={{width :"100vw", height: "100vh"}}>
+            Elimina datos de usuario
+        </div>
+    )
+}
+
+export default deleteFacebook
